341fbdbc8fb35cbab2691dbc8757dfa62f927a5e
ximenyuejun <163388661+ximenyuejun@users.noreply.github.com>
2025-06-30 13:37:26 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,12 @@
 from ultralytics import YOLO
 
-# 加载模型
-model = YOLO("runs/detect/train18/weights/best.pt")  # 加载预训练模型（建议用于训练）
+# Load model
+model = YOLO("model/original+noCBAM+CIoU.pt") 
 
-# 使用模型
-model.val(data="datasets/PCB_DATASET_Contrast_000_0/test.yaml") 
-#model.val(data="datasets/PCB_DATASET_Original/PCB_test.yaml")  # 测试模型
-model.val(data="datasets/PCB_DATASET_Contrast_003_5/test.yaml")  # 测试模型
-model.val(data="datasets/PCB_DATASET_Contrast_003_10/test.yaml")  # 测试模型
-model.val(data="datasets/PCB_DATASET_Contrast_006_5/test.yaml")  # 测试模型
-model.val(data="datasets/PCB_DATASET_Contrast_006_10/test.yaml") 
-#model.val(data="datasets/PCB_DATASET_Shear_Rotate_Seg_Guass/PCB_test.yaml")  # 测试模型
+# Inference
+model.val(data="datasets/PCB_DATASET_Contrast_000_0/test.yaml")  
+# model.val(data="datasets/PCB_DATASET_Contrast_003_5/test.yaml")  
+# model.val(data="datasets/PCB_DATASET_Contrast_003_10/test.yaml") 
+# model.val(data="datasets/PCB_DATASET_Contrast_006_5/test.yaml")  
+# model.val(data="datasets/PCB_DATASET_Contrast_006_10/test.yaml")
 
